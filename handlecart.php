a7e807021239ccc095b622f0da4005295d501098
RookieDookie01 <ashrfwjdii@gmail.com>
2024-06-20 14:51:18 +0800

    251

--- handlecart.php
+++ handlecart.php
@@ -6,17 +6,17 @@
     if(isset($_SESSION['auth']))
     {
         if(isset($_GET['t']))
-    {
-        $product_id = $_GET['t'];
-        $user_id = $_SESSION['auth_user']['user_id'];
-
-        $insert_query = "INSERT INTO carts (user_id, product_id) VALUES ('$user_id', '$product_id')";
-        $insert_query_run = mysqli_query($con, $insert_query);
-        if($insert_query_run)
         {
-            echo $_SESSION['message'] = "Item Added To Cart";
+            $product_id = $_GET['t'];
+            $user_id = $_SESSION['auth_user']['user_id'];
+
+            $insert_query = "INSERT INTO carts (user_id, product_id) VALUES ('$user_id', '$product_id')";
+            $insert_query_run = mysqli_query($con, $insert_query);
+            if($insert_query_run)
+            {
+                echo $_SESSION['message'] = "Item Added To Cart";
+            }
         }
-    }
     }
     else
     {
